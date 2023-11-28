513058e4011a8d2eb84874346d6b9674fb26d3ad
CarvinJ <113186066+CarvinJ@users.noreply.github.com>
2023-11-28 13:20:45 -0500

    3.2 done

--- Assets/Scripts/SpawnManger.cs
+++ Assets/Scripts/SpawnManger.cs
@@ -8,10 +8,13 @@ public class SpawnManger : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript =
+        GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -22,7 +25,10 @@ public class SpawnManger : MonoBehaviour
 
     void SpawnObstacle ()  
     {
-        Instantiate(obstaclePrefeb, spawnPos, obstaclePrefeb.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefeb, spawnPos, obstaclePrefeb.transform.rotation);
+        }
     }
     
 }
